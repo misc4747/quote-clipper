03809a568695b837981ed86c71111f4a2feb13aa
misc4747 <miscvd@gmail.com>
2023-06-05 21:21:29 +0900

    refactor: 新たにhandleQuoteClipperを作って、重複部分を消す

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 const updateContextMenus = async () => {
   await chrome.contextMenus.removeAll();
   chrome.contextMenus.create({
-      id: "quote-select-text",
-      title: "選択範囲を引用形式でコピー",
-      contexts: ["all"]
+    id: "quote-select-text",
+    title: "選択範囲を引用形式でコピー",
+    contexts: ["all"]
   });
 };
 
@@ -11,35 +11,29 @@ chrome.runtime.onInstalled.addListener(updateContextMenus);
 chrome.runtime.onStartup.addListener(updateContextMenus);
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {message: 'getSelectionText'}, function(response) {
-      //console.log(response);
-      let text = toQuoteFormat(response);
-      toClipboard(tabs[0], text);
-    });
-  });
+  handleQuoteClipper();
 });
 
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'quote-clipper') {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: 'getSelectionText'}, function(response) {
-        //console.log(response);
-        let text = toQuoteFormat(response);
-        toClipboard(tabs[0], text);
-      });
-    });
+    handleQuoteClipper();
   }
 });
+
+const handleQuoteClipper = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {message: 'getSelectionText'}, function(response) {
+      let text = toQuoteFormat(response);
+      toClipboard(tabs[0], text);
+    });
+  });
+};
 
 const toClipboard = (tab, text) => {
   function injectedFunction(text) {
     try {
       navigator.clipboard.writeText(text);
-      //console.log('success');
-    } catch (e) {
-      //console.log('failed');
-    }
+    } catch (e) {}
   }
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
@@ -52,4 +46,3 @@ const toQuoteFormat = (text) => {
   let replaced = text.replace(/\n/g, '\n> ');
   return '> ' + replaced;
 };
-
